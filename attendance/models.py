f2c57cdf2b67d7c4fc2c2f897b2ce75ce93354d5
jacob <jacobja3@gmail.com>
2018-01-08 22:32:54 +0530

    Second Commit

--- attendance/models.py
+++ attendance/models.py
@@ -14,16 +14,24 @@ class employee(models.Model):
     PART_TIME_STAFF='PF'
     OFFICE_TRAINEE='OT'
     DEPT_TRAINEE='DT'
+     
 
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    def __unicode__(self):
+        return unicode(self.user) 
     
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+
     STAFF_CHOICES=(
       (REGULAR_ACADEMIC,'Regular-Academic'),
       (REGULAR_NON_ACADEMIC,'Regular-NonAcademic'),
       (CONTRACT_STAFF_1_YR,'Contract Staff(1 year)'),
       (CONTRACT_STAFF_6_MNTH,'Contract Staff (6 months)'),
-
+      (AD_HOC_STAFF,'Ad-Hoc Staff'),
+      (PART_TIME_STAFF,'Part-time-staff'),
+      (OFFICE_TRAINEE,'Office Trainee'),
+      (DEPT_TRAINEE,'Department Trainee')
     )
 
+    category=models.CharField(max_length=3,choices=STAFF_CHOICES,default=REGULAR_ACADEMIC)
 
-    )
+
