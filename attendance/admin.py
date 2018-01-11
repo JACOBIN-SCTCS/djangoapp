7f9b949f2f2bf8a42eb9232be5bd763761eba98f
jacob <jacobja3@gmail.com>
2018-01-11 22:27:20 +0530

    Updated models

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import employee
+from .models import staff_category,staff,leave
 
 # Register your models here.
-admin.site.register(employee)
+admin.site.register(staff_category)
+admin.site.register(staff)
+admin.site.register(leave)
--- attendance/models.py
+++ attendance/models.py
@@ -1,37 +1,72 @@
 from django.db import models
 from django.contrib.auth.models import User 
 
-# Create your models here.
+
+categories =(
+
+    ('RA','Regular Academic'),
+    ('RNA','Regular Non-Academic'),
+    ('C1','Contract Staff (1 yr)'),
+    ('C6','Contract Staff (6 month)'),
+    ('AS','Ad-hoc Staff'),
+    ('PF','Part time Staff'),
+    ('OT','Office Trainee'),
+    ('DT','Department Trainee')
+)
+
+departments =(
 
 
-class employee(models.Model):
+    ('BT','BioTechnology'),
+    ('CSE','Computer Science'),
+    ('EC','Electronics and Communication'),
+    ('MECH','Mechanical'),
+    ('APPL' ,'Applied Science'),
+    ('LIB','Library'),
+    ('ADM','Administration'),
+    ('OTH','Others')
+)
 
-    REGULAR_ACADEMIC='RA'
-    REGULAR_NON_ACADEMIC='RNA'
-    CONTRACT_STAFF_1_YR ='C1'
-    CONTRACT_STAFF_6_MNTH='C6'
-    AD_HOC_STAFF='AS'
-    PART_TIME_STAFF='PF'
-    OFFICE_TRAINEE='OT'
-    DEPT_TRAINEE='DT'
-     
+class staff_category(models.Model):
 
-    def __unicode__(self):
-        return unicode(self.user) 
-    
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.category
 
-    STAFF_CHOICES=(
-      (REGULAR_ACADEMIC,'Regular-Academic'),
-      (REGULAR_NON_ACADEMIC,'Regular-NonAcademic'),
-      (CONTRACT_STAFF_1_YR,'Contract Staff(1 year)'),
-      (CONTRACT_STAFF_6_MNTH,'Contract Staff (6 months)'),
-      (AD_HOC_STAFF,'Ad-Hoc Staff'),
-      (PART_TIME_STAFF,'Part-time-staff'),
-      (OFFICE_TRAINEE,'Office Trainee'),
-      (DEPT_TRAINEE,'Department Trainee')
-    )
-
-    category=models.CharField(max_length=3,choices=STAFF_CHOICES,default=REGULAR_ACADEMIC)
+    category=models.CharField(max_length=3,choices=categories ,default='RA')
+    max_casual_leave=models.IntegerField()
+    max_compensation_leave=models.IntegerField()
+    max_earned_leave=models.IntegerField()
+    max_half_pay_leave=models.IntegerField()
+    max_leave_with_allowance=models.IntegerField()
+    max_duty_leave=models.IntegerField()
 
 
+class staff(models.Model):
+
+    def __str__(self):
+         return self.staff_id
+
+
+    staff_id=models.IntegerField(primary_key=True)
+    name=models.CharField(max_length=30 ,default="employee")
+    category=models.ForeignKey(staff_category,on_delete=None)
+    department=models.CharField(max_length=5,choices=departments,default='OTH')
+    qualification=models.CharField(max_length=30,blank=True)
+    joining_date=models.DateField(default=None)
+    termination_date=models.DateField(default=None,blank=True)
+
+
+class leave(models.Model):
+    def __str__(self):
+        return self.emp_id.staff_id
+
+
+
+    emp_id=models.ForeignKey(staff,on_delete=models.CASCADE)
+    present_days=models.IntegerField(default=0)
+    casual_leave = models.IntegerField(default=0)
+    compensation_leave = models.IntegerField(default=0)
+    earned_leave= models.IntegerField(default=0)
+    half_pay_leave = models.IntegerField(default=0)
+    leave_allowance= models.IntegerField(default=0)
+    duty_leave = models.IntegerField(default=0)
